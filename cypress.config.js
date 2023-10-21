a4462f86b2a0dcec4b37efbb414cfd037fca3cc6
YuMZyX <joni1koskinen@gmail.com>
2023-10-21 22:43:27 +0300

    Adding frontend code

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: '41grhw',
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) { // eslint-disable-line no-unused-vars
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:5173',
+    env: {
+      BACKEND: 'http://localhost:3003/api',
+    },
+  },
+})
